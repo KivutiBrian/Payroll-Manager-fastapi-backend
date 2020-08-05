216dfc9e2cea9f00093618a62e4bbd67801ec620
kivutibrian <briankivuti1@gmail.com>
2020-08-05 22:21:54 +0300

    employee service

--- app/services/employee_service.py
+++ app/services/employee_service.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from models.employee_model import Employee
+from schemas.employee_schema import CreateEmployee
 
 class EmployeeService:
 
@@ -19,16 +20,31 @@ class EmployeeService:
         return record
 
     @staticmethod
-    def create_employee(data, db:Session):
+    def create_employee(data:CreateEmployee, db:Session):
         """create a new employee"""
-        pass
+        email = db.query(Employee).filter(Employee.email==data.email).first()
+        if email:
+            raise HTTPException(status_code=400, detail='email taken!')
+        
+        # save to database
+        record = Employee(**data.dict())
+        db.add(record)
+        db.commit()
+        db.refresh()
+        return record
+
 
     @staticmethod
     def update_employee_record(data,employee_id:int, db:Session):
         """update employee record"""
         pass
 
+
     @staticmethod
     def deactivate_employee(employee_id:int, db:Session):
         """deactivate an employee"""
-        pass
+        record = db.query(Employee).filter(Employee.id == employee_id ).first()
+        # set the active status to false
+        record.active = False
+        db.commit()
+        return record
